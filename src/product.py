e64488f981950b66e9ee99f4017632e17c1691a7
Olga Kazorina <nefertituolga@gmail.com>
2025-02-18 13:19:30 +0300

    add fixes src/product.py

--- src/product.py
+++ src/product.py
@@ -1,10 +1,14 @@
+from pydantic import BaseModel, Field
+from decimal import Decimal
+
+
 class Product:
     """Класс для представления продукта"""
 
     name: str
     description: str
-    price: float
-    quantity: int
+    price: Decimal = Field(..., gt=0)  # Цена должна быть больше, чем 0
+    quantity: int = Field(..., ge=0)  # Количество должно быть больше или равно 0
 
     def __init__(self, name, description, price, quantity):
         """
