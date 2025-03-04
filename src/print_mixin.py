60a45c46471aad55f8fa59d569e77bfade053431
Olga Kazorina <nefertituolga@gmail.com>
2025-03-04 18:40:46 +0300

    add fixes src/print_mixin.py

--- src/print_mixin.py
+++ src/print_mixin.py
@@ -3,15 +3,11 @@ class PrintMixin:
     Mixin, добавляющий функциональность печати информации о продукте
     """
 
-    def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
+    def __init__(self) -> None:
         """Инициализирует PrintMixin"""
-        self.name = name
-        self. description = description
-        self.price = price
-        self.quantity = quantity
         print(repr(self))
 
     def __repr__(self) -> str:
         """Возвращает строковое представление объекта PrintMixin
         для печати в консоль"""
-        return f"{self.__class__.__name__}('{self.name}', '{self.description}', {self.price}, {self.quantity})"
+        return f"{self.__class__.__name__}('{self.name}', '{self.description}', {self.price}, {self.quantity})"   #type: ignore
